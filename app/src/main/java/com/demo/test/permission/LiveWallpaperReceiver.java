0ef243eeec5098fb12c22140d65b83821e2d6df1
lijia <lijia@apusapps.com>
2018-07-11 10:56:13 +0800

    初始化资源主题apk

---
+++ app/src/main/java/com/demo/test/permission/LiveWallpaperReceiver.java
@@ -0,0 +1,15 @@
+package com.demo.test.permission;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * @author lijia
+ */
+public class LiveWallpaperReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
